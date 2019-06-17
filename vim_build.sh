cf187d0b3ff401cae29ebd653d45de15dacf9127
yskaksk <yskaksk@gmail.com>
2019-06-18 01:41:12 +0900

    誤字の修正

--- vim_build.sh
+++ vim_build.sh
@@ -2,6 +2,6 @@ sourcedir='/Users/yosukeakasaka/Documents/github/vim'
 
 cd $sourcedir
 
-./configure --with-futures=huge --enable-python3interp=yes --enable-luainterp=yes --enable-terminal=yes --enable-if-missing=yes
+./configure --with-features=huge --enable-python3interp=yes --enable-luainterp=yes --enable-terminal=yes --enable-fail-if-missing=yes
 make
 sudo make install
